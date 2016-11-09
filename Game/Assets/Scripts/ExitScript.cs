9cf973db29cd686d246941430a3a7f6e67a34a47
RianF2 <rianfitz@gmail.com>
2016-11-09 16:03:50 +0000

    initial commit

---
+++ Game/Assets/Scripts/ExitScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void onClick()
+	{
+		Debug.Log("Time to exit the game");
+		Application.Quit();
+	}
+}
