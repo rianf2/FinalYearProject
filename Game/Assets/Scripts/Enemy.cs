0ae5670b4d6858827601323332bf85f7c6afd599
RianF2 <rianfitz@gmail.com>
2017-01-06 11:32:32 +0000

    Lets hope this works

--- Game/Assets/Scripts/Enemy.cs
+++ Game/Assets/Scripts/Enemy.cs
@@ -3,20 +3,22 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour {
 
-	private float shootProbability = Random.value;
-	public GameObject projectile;
+	private float shootProbability;
+	//public GameObject projectile;
 	// Use this for initialization
 	void Start () {
 		Debug.Log("HELLO FROM ENEMY SCRIPT");
+		shootProbability = Random.value;
 	}
 	
 	// Update is called once per frame
 	void Update () { 
 		if(shootProbability < 0.75)
 		{
-			Vector3 startPos = transform.position + new Vector3(0, -1, 0);
-			GameObject missile = Instantiate(projectile, startPos, Quaternion.identity) as GameObject;
-			missile.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -10f);
+			Debug.Log("Shooting a laser");
+			//Vector3 startPos = transform.position + new Vector3(0, -1, 0);
+			//GameObject missile = Instantiate(projectile, startPos, Quaternion.identity) as GameObject;
+			//missile.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -10f);
 		}
 	}
 }
--- Game/Assets/Scripts/PlayerController.cs
+++ Game/Assets/Scripts/PlayerController.cs
@@ -8,34 +8,50 @@ public class PlayerController : MonoBehaviour {
 	 * creates smoother movement of the player
 	 */
 	private Vector3 movementVector;
+	private float speed = 15.0f;
 	/*
 	 * Use these at later date for stopping ship when it hits
 	 * the left most and right most part of the screen
 	 */
-	private int xMin, xMax; 
+	private float xMin, xMax; 
 
 	// Use this for initialization
 	void Start () {
-		movementVector = new Vector3(1, 0, 0);
+		float dist = transform.position.z - Camera.main.transform.position.z;
+
+		Vector3 leftBound = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, dist));
+		Vector3 rightBound = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, dist));
+
+		xMin = leftBound.x + 0.6f; //finetuned variable
+		xMax = rightBound.x - 0.6f;
+
+		Debug.Log("Minimum x value: " + xMin);
+		Debug.Log("Maximum x value: " + xMax);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
-		//switch styatemant???
-		if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+		/*
+		 * For report: there was a problem here. Input.GetKeyDown only 
+		 * registered when key was pressed (duh) ond only moved one unit.
+		 * When changed to Input.GetKey, allowed for fluid movement. Think
+		 * of better way to write this for report
+		 */
+		if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
 		{
-			transform.position -= movementVector;
-			Debug.Log("Key pressed: A");
+			transform.position += Vector3.left * speed * Time.deltaTime;
 		}
-		else if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+		else if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
 		{
-			Debug.Log("Key pressed: D");
-			transform.position += movementVector;
+			transform.position += Vector3.right * speed * Time.deltaTime;
 		}
 		else if(Input.GetKeyDown(KeyCode.Space))
 		{
 			Debug.Log("FIRE");
 		}
+
+		float gamespace = Mathf.Clamp(transform.position.x, xMin, xMax);
+		transform.position = new Vector3(gamespace, transform.position.y, transform.position.z);
 	}
 }
